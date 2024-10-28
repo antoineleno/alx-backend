cdfa8d0de6657a9a770ffdb005ae7bea2f467687
antoineleno <johndoe@example.com>
2024-10-28 22:33:02 +0800

    Pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""0-simple helper function module"""
+
+
+def index_range(page, page_size):
+    """Index range function"""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
---
+++ 0x00-pagination/REAMDE.md
@@ -0,0 +1,3 @@
+# 0x00-pagination
+* [REST API Design: Pagination](https://www.moesif.com/blog/technical/api-design/REST-API-Design-Filtering-Sorting-and-Pagination/#pagination)
+* [HATEOAS](https://en.wikipedia.org/wiki/HATEOAS)
