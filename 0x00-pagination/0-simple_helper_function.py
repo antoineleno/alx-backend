ba4b831e6dc7463852490eea8e10b4e01686587f
antoineleno <johndoe@example.com>
2024-10-28 22:42:16 +0800

    Pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """0-simple helper function module"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Index range function"""
     start = (page - 1) * page_size
     end = start + page_size
