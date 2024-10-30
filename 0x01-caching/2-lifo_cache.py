2f592f29bdb963a0446210923cc695ccd8db2845
antoineleno <johndoe@example.com>
2024-10-31 01:41:10 +0800

    0x01 Caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""LIFO caching module.
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOCache that inherits from BaseCaching
+    and is a caching system for last in and First out
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            last_value = list(self.cache_data.keys())[-2]
+            print("DISCARD: {}".format(last_value))
+            del self.cache_data[last_value]
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
+    
