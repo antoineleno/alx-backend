526d6c1de98bbdd6a858b4271ddd87b60959f90b
antoineleno <johndoe@example.com>
2024-10-30 19:35:47 +0800

    0x01 Caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,10 +8,9 @@ class BasicCache(BaseCaching):
     """BasicCache Class"""
     def put(self, key, item):
         """Add an iems to the cache"""
-        if key is not None or item is not None:
-            self.cache_data[key] = item
-        else:
+        if key is None or item is None:
             return
+        self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key from the cache"""
