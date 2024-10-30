bb111430770e6718fb958f526cbc832964df9fa2
antoineleno <johndoe@example.com>
2024-10-30 19:24:54 +0800

    Caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Base cache module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache Class"""
+    def put(self, key, item):
+        """Add an iems to the cache"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key from the cache"""
+        if key is None:
+            return None
+
+        for key in self.cache_data:
+            if key == key:
+                return self.cache_data[key]
+            else:
+                return None
