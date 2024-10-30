bd40f00e5a13643f293df5f178db2db5c909901c
antoineleno <johndoe@example.com>
2024-10-30 19:42:06 +0800

    0x01 Caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,21 +1,21 @@
-#!/usr/bin/python3
-""" Base cache module
+#!/usr/bin/env python3
+"""Basic caching module.
 """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """BasicCache Class that allow to store and get an item
-    from a cache memory
+    """BasicCache emthood that allows storing and
+    retrieving items from a dictionary.
     """
     def put(self, key, item):
-        """Add an iems to the cache"""
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        """Get an item by key from the cache"""
-        self.cache_data.get(key, None)
-
-
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
